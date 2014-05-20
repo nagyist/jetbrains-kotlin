a78764d0d14ba8debcd9369f1726c2ef92791684
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2014-05-20 20:17:31 +0400

    Fixed test data

--- compiler/testData/diagnostics/tests/extensions/extensionMemberInClassObject.kt
+++ compiler/testData/diagnostics/tests/extensions/extensionMemberInClassObject.kt
@@ -1,5 +1,7 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 
+//todo Fix this test in IDE
+
 trait JPAEntityClass<D> {
     fun <T> T.findByName(s: String): D {null!!}
 }
@@ -9,9 +11,9 @@ class Foo {
 }
 
 fun main(args: Array<String>) {
-    <!TYPE_INFERENCE_TYPE_CONSTRUCTOR_MISMATCH!>with<!>("", {
+    <!TYPE_INFERENCE_TYPE_CONSTRUCTOR_MISMATCH!>with<!>("", <!TYPE_MISMATCH!>{
         Foo.<!MISSING_RECEIVER, TYPE_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>findByName<!>("")
-    })
+    }<!>)
 }
 
 fun <T> with(t: T, f: T.() -> Unit) {}
