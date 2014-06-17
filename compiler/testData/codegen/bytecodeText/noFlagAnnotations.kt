86c9fafd2787068921701ed681a4e7fd44e6bcbf
Andrey Breslav <andrey.breslav@jetbrains.com>
2014-06-17 20:17:10 +0400

    Support transient, strictfp and synchronized flags
     #KT-4377 Fixed

---
+++ compiler/testData/codegen/bytecodeText/noFlagAnnotations.kt
@@ -0,0 +1,12 @@
+volatile var vol = 1
+transient val tra = 1
+
+native fun nat() {}
+strictfp fun str() {}
+synchronized fun sync() {}
+
+// 0 kotlin/jvm/volatile
+// 0 kotlin/jvm/transient
+// 0 kotlin/jvm/native
+// 0 kotlin/jvm/strictfp
+// 0 kotlin/jvm/synchronized
