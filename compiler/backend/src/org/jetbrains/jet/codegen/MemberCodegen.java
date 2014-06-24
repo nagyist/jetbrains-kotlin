hhariri/kotlin

Construct reflection objects via static factory methods
JVM back-end now generates invocations of these methods instead of
KClass/K*Property/... constructors for two reasons:

1. It occupies less space in the bytecode
2. We can (to some degree) alter the implementation of the factory methods
   without changing the ABI compatibility version

compiler/backend/src/org/jetbrains/jet/codegen/MemberCodegen.java
@@ -43,6 +43,7 @@ import org.jetbrains.jet.storage.NotNullLazyValue;
 import org.jetbrains.org.objectweb.asm.MethodVisitor;
 import org.jetbrains.org.objectweb.asm.Type;
 import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter;
+import org.jetbrains.org.objectweb.asm.commons.Method;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -323,21 +324,19 @@ public abstract class MemberCodegen<T extends JetElement/* TODO: & JetDeclaratio
             @NotNull GenerationState state,
             @NotNull Type thisAsmType,
             @NotNull ClassBuilder classBuilder,
-            @NotNull Type kImplType,
+            @NotNull Method factory,
             @NotNull String fieldName,
             @NotNull InstructionAdapter v
     ) {
+        String desc = factory.getReturnType().getDescriptor();
         // TODO: generic signature
-        classBuilder.newField(NO_ORIGIN, ACC_PUBLIC | ACC_STATIC | ACC_FINAL | ACC_SYNTHETIC, fieldName, kImplType.getDescriptor(),
-                              null, null);
+        classBuilder.newField(NO_ORIGIN, ACC_PUBLIC | ACC_STATIC | ACC_FINAL | ACC_SYNTHETIC, fieldName, desc, null, null);
 
         if (state.getClassBuilderMode() == ClassBuilderMode.LIGHT_CLASSES) return;
 
-        v.anew(kImplType);
-        v.dup();
         v.aconst(thisAsmType);
-        v.invokespecial(kImplType.getInternalName(), "<init>", "(Ljava/lang/Class;)V", false);
-        v.putstatic(thisAsmType.getInternalName(), fieldName, kImplType.getDescriptor());
+        v.invokestatic(REFLECTION_INTERNAL_PACKAGE, factory.getName(), factory.getDescriptor(), false);
+        v.putstatic(thisAsmType.getInternalName(), fieldName, desc);
     }
 
     protected void generatePropertyMetadataArrayFieldIfNeeded(@NotNull Type thisAsmType) {

nagyist/jetbrains-kotlin

compiler/backend/src/org/jetbrains/jet/codegen/MemberCodegen.java
@@ -43,6 +43,7 @@ import org.jetbrains.jet.storage.NotNullLazyValue;
 import org.jetbrains.org.objectweb.asm.MethodVisitor;
 import org.jetbrains.org.objectweb.asm.Type;
 import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter;
+import org.jetbrains.org.objectweb.asm.commons.Method;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -320,21 +321,19 @@ public abstract class MemberCodegen<T extends JetElement/* TODO: & JetDeclaratio
             @NotNull GenerationState state,
             @NotNull Type thisAsmType,
             @NotNull ClassBuilder classBuilder,
-            @NotNull Type kImplType,
+            @NotNull Method factory,
             @NotNull String fieldName,
             @NotNull InstructionAdapter v
     ) {
+        String type = factory.getReturnType().getDescriptor();
         // TODO: generic signature
-        classBuilder.newField(NO_ORIGIN, ACC_PUBLIC | ACC_STATIC | ACC_FINAL | ACC_SYNTHETIC, fieldName, kImplType.getDescriptor(),
-                              null, null);
+        classBuilder.newField(NO_ORIGIN, ACC_PUBLIC | ACC_STATIC | ACC_FINAL | ACC_SYNTHETIC, fieldName, type, null, null);
 
         if (state.getClassBuilderMode() == ClassBuilderMode.LIGHT_CLASSES) return;
 
-        v.anew(kImplType);
-        v.dup();
         v.aconst(thisAsmType);
-        v.invokespecial(kImplType.getInternalName(), "<init>", "(Ljava/lang/Class;)V", false);
-        v.putstatic(thisAsmType.getInternalName(), fieldName, kImplType.getDescriptor());
+        v.invokestatic(REFLECTION_INTERNAL_PACKAGE, factory.getName(), factory.getDescriptor(), false);
+        v.putstatic(thisAsmType.getInternalName(), fieldName, type);
     }
 
     protected void generatePropertyMetadataArrayFieldIfNeeded(@NotNull Type thisAsmType) {